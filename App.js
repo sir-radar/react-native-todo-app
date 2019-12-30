720d815f1fa41101fb07f7cee235aec1a54d3bd5
MAC <nwokikesamson@gmail.com>
2019-12-30 04:57:07 +0100

    added headed and todo list item components

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+//components
+import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'Buy coffee', key: '1'},
+    {text: 'Create an app', key: '2'},
+    {text: 'Play on the switch', key: '3'},
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header/>
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => (
+             <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,7 +39,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content:{
+    padding: 40
+  },
+  list:{
+    marginTop: 20
+  }
 });
