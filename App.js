5864502a0ec8cdf631aeb8ba10c47f5231b16077
MAC <nwokikesamson@gmail.com>
2019-12-30 12:37:41 +0100

    added flex property for better display

--- App.js
+++ App.js
@@ -58,9 +58,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   content:{
-    padding: 40
+    padding: 40,
+    flex: 1
   },
   list:{
-    marginTop: 20
+    marginTop: 20,
+    flex: 1
   }
 });
